30799602fd8098e91a20cd79ad79112c02029c79
AlexZ1967 <alexz1967@gmail.com>
2023-08-21 20:08:33 +0300

    no final: with bugs

--- lin_system.cpp
+++ lin_system.cpp
@@ -7,6 +7,7 @@ int main()
 	
 {
     double a, b, c, d, e, f;
+
     cin>>a>>b>>c>>d>>e>>f;
 
     double det = a*d - c*b;
@@ -14,8 +15,36 @@ int main()
     double det_x = e*d - f*b;
     double det_y = a*f - c*e;
 
-    double x = det_x/det;
-    double y = det_y/det;
-    cout << x << " " << y << endl;
+    if (det){
+        double x = det_x/det;
+        double y = det_y/det;
+        cout<< 2 << " " << x << " " << y << endl;
+        return 0;
+    }
+    if (det_x == 0 && det_y == 0 && a*b*e && c*d*f) {
+        double k, n;
+        if (a < c) {
+            k = -a/b;
+            n = e/b;
+        } else {
+            k = -c/d;
+            n = f/d;
+        }
+        cout << 1 << " " << k << " " << n << endl;
+        return 0;
+    }
+    if (det_x == 0) {
+        double x0 = e/a;
+        cout << 3 << " " << x0 << endl;
+        return 0;
+    }
+    if (det_y == 0) {
+        double y0 = f/c;
+        cout << 4 << " " << y0 << endl;
+        return 0;
+    }
+
+    cout << 5 <<endl;
+
     return 0;
 }
