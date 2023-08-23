615815723adc8cf55ea8d3d64c626f01ea018925
AlexZ1967 <AlexZ1967@gmail.com>
2023-08-24 02:10:48 +0300

    add powerf func

--- power.cpp
+++ power.cpp
@@ -8,16 +8,26 @@
 int power(int x, unsigned p) {
     int answer = 1;
     while(p){
-      answer *= x;
-      p--;
+    answer *= x;
+    p--;
+    }
+    return answer;
+}
+
+double powerf(double x, unsigned p) {
+    double answer = 1;
+    while(p){
+    answer *= x;
+    p--;
     }
     return answer;
 }
 
 int main(){
-  int a;
-  unsigned b;
-  std::cin>>a>>b;
-  std::cout<<power(a, b)<<std::endl;;
-  return 0;
+    int a;
+    unsigned b;
+    std::cin>>a>>b;
+    std::cout<<power(a, b)<<std::endl;
+    std::cout<<powerf(a, b)<<std::endl;
+    return 0;
 }
